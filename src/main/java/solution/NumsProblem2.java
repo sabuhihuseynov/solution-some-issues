0225c54587339e5b8c9cf08b3f2898634e1837cb
sabuhihuseynov <huseynovsebuhi14@gmail.com>
2024-01-23 12:48:24 +0400

    adding new solution

--- src/main/java/solution/NumsProblem2.java
+++ src/main/java/solution/NumsProblem2.java
@@ -1,8 +1,10 @@
 package solution;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 public class NumsProblem2 {
 
@@ -239,5 +241,23 @@ public class NumsProblem2 {
         }
         return arr;
     }
+
+    public int[] decompressRLElist(int[] nums) {
+        int arrSize = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            arrSize += nums[i];
+        }
+
+        int[] result = new int[arrSize];
+
+        int startIdx = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            Arrays.fill(result, startIdx, startIdx + nums[i], nums[i + 1]);
+            startIdx += nums[i];
+        }
+        return result;
+    }
+
+
 
 }
