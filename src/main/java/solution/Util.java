daf85178e612c2d85b9cc7e58e124074a3b9f39b
Sabuhi Huseynov <sabuhi.huseynov@guavapay.com>
2024-06-03 16:18:35 +0400

    Added new method to the util class

---
+++ src/main/java/solution/Util.java
@@ -0,0 +1,19 @@
+package solution;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+public class Util {
+
+    public static HashMap<String, String> findCountryIso2Code() {
+        String[] isoCountries = Locale.getISOCountries();
+        HashMap<String, String> countries = new HashMap<>(isoCountries.length);
+        for (String country : isoCountries) {
+            Locale locale = new Locale("", country);
+            countries.put(locale.getISO3Country().toUpperCase(), locale.getCountry());
+        }
+        countries.put("GBR", "UK");
+        return countries;
+    }
+
+}
