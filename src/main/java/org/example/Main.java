38df6fef868d25cc0995a5322f310ead4f03598e
Sabuhi Huseynov <sabuhi.huseynov@guavapay.com>
2024-11-16 19:36:05 +0400

    lesson 2 tasks done!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,9 @@
 package org.example;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("adwd");
+        System.out.println("Hello world!");
 
     }
 }
