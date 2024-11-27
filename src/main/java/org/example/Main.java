548942f495fed44fbe1210d108bbcee1e5c8818d
Sabuhi Huseynov <sabuhi.huseynov@guavapay.com>
2024-11-27 12:37:58 +0400

    added day 2 leetcode problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.solution.nums_problems.NumsMediumProblems;
+
 public class Main {
     public static void main(String[] args) {
-
+        NumsMediumProblems numsMediumProblems = new NumsMediumProblems();
+        int[][] queries = {{2, 4}, {0, 2}, {0, 4}};
+        int[] result = numsMediumProblems.shortestDistanceAfterQueries(5, queries);
+        for (int res : result) {
+            System.out.println(res);
+        }
     }
 }
--- src/main/java/org/example/solution/nums_problems/NumsMediumProblems.java
+++ src/main/java/org/example/solution/nums_problems/NumsMediumProblems.java
@@ -1,6 +1,8 @@
 package org.example.solution.nums_problems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class NumsMediumProblems {
 
@@ -90,6 +92,54 @@ public class NumsMediumProblems {
             }
         }
         return 0;
+    }
+
+    /**
+     * <b>MEDIUM</b>
+     * 10 ms 99.03%
+     * <a href="https://leetcode.com/problems/shortest-distance-after-road-addition-queries-i/description/?envType=daily-question&envId=2024-11-27"> Shortest Distance After Road Addition Queries I</a>
+     **/
+    public int[] shortestDistanceAfterQueries(int n, int[][] queries) {
+        int[] distances = new int[n];
+        for (int i = 0; i < n; ++i) {
+            distances[i] = n - 1 - i;
+        }
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i + 1 < n; ++i) {
+            graph.get(i + 1).add(i);
+        }
+
+        int[] answer = new int[queries.length];
+        int queryIdx = 0;
+
+        for (int[] query : queries) {
+            int source = query[0];
+            int target = query[1];
+
+            graph.get(target).add(source);
+            distances[source] = Math.min(distances[source], distances[target] + 1);
+            updateDistances(graph, source, distances);
+
+            answer[queryIdx++] = distances[0];
+        }
+
+        return answer;
+    }
+
+    private void updateDistances(List<List<Integer>> graph, int current, int[] distances) {
+        int newDist = distances[current] + 1;
+
+        for (int neighbor : graph.get(current)) {
+            if (distances[neighbor] <= newDist) continue;
+
+            distances[neighbor] = newDist;
+            updateDistances(graph, neighbor, distances);
+        }
     }
 
 
