77bbd6ab48e902edf57a1552bc386ba46fb4f137
Sabuhi Huseynov <sabuhi.huseynov@guavapay.com>
2025-02-11 15:40:50 +0400

    added some solutions

--- src/main/java/org/example/solution/nums_problems/NumsMediumProblems.java
+++ src/main/java/org/example/solution/nums_problems/NumsMediumProblems.java
@@ -143,5 +143,44 @@ public class NumsMediumProblems {
         }
     }
 
+    /**
+     * <b>MEDIUM</b>
+     * 0 ms 100.00%
+     * <a href="https://leetcode.com/problems/rotate-array/"> Rotate Array</a>
+     **/
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    /**
+     * <b>MEDIUM</b>
+     * 0 ms 100.00%
+     * <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/"> Best Time to Buy and Sell Stock II</a>
+     **/
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+
 
 }
--- src/main/java/org/example/util/CommonUtil.java
+++ src/main/java/org/example/util/CommonUtil.java
@@ -2,14 +2,15 @@ package org.example.util;
 
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class CommonUtil {
 
-    public static HashMap<String, String> findCountryIso2Code() {
+    public static Map<String, String> findCountryIso2Code() {
         String[] isoCountries = Locale.getISOCountries();
-        HashMap<String, String> countries = new HashMap<>(isoCountries.length);
+        HashMap<String, String> countries = HashMap.newHashMap(isoCountries.length);
         for (String country : isoCountries) {
-            Locale locale = new Locale("", country);
+            Locale locale = Locale.of("", country);
             countries.put(locale.getISO3Country().toUpperCase(), locale.getCountry());
         }
         countries.put("GBR", "UK");
